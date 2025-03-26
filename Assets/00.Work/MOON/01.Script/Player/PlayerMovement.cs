4b420f0fe04afa8b920673276dc52b0adeb60bd2
unknown <anstjdalsdlek@gmail.com>
2025-03-26 17:27:59 +0900

    feat:PlayerState

--- Assets/00.Work/MOON/01.Script/Player/PlayerMovement.cs
+++ Assets/00.Work/MOON/01.Script/Player/PlayerMovement.cs
@@ -1,35 +1,56 @@
 using System;
+using System.Collections.Generic;
+using _00.Work.MOON._01.Script.SO;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _00.Work.MOON._01.Script.Player
 {
     public class PlayerMovement : MonoBehaviour, IEntityComponent
     {
-        [SerializeField] float moveSpeed = 5f;
-        [SerializeField] float jumpPower = 5f;
-        [SerializeField] float maxSpeed;
+        [field: SerializeField]PlayerMovementTypeSO[] typeList;
+        private Dictionary<MovementType, PlayerMovementTypeSO> _typeDictionary =
+            new Dictionary<MovementType, PlayerMovementTypeSO>();
+        private PlayerMovementTypeSO _currentType;
+        public float JumpPower => _currentType.jumpPower;
+        public float MoveSpeed => _currentType.moveSpeed;
+        public float MaxSpeed => _currentType.maxSpeed;
         public bool IsMaxed { get; private set; }
         private Entity _entity;
         private Rigidbody _rb;
-        private Vector2 moveDir;
+        private Vector2 _moveDir;
         private void Awake()
         {
             _rb = GetComponent<Rigidbody>();
+            foreach (PlayerMovementTypeSO type in typeList)
+            {
+                _typeDictionary.Add(type.Type, type);
+            }
+            ChangeType(MovementType.Normal);
+        }
+
+        public void ChangeType(MovementType type)
+        {
+            _currentType = _typeDictionary[type];
+            // if (_currentType.timer > 0)
+            // {
+            //     
+            // }
         }
 
         public void Initialize(Entity entity)
-        {
+        {    
             _entity = entity;
         }
         public void MoveDirChanged(Vector2 change)
         {
-            moveDir = change;
+            _moveDir = change;
         }
         public void Jump()
         {
             if (_entity.GetCompo<GroundChecker>().GroundCheck())
             {
-                _rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+                _rb.AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
             }
         }
         private void FixedUpdate()
@@ -44,14 +65,14 @@ namespace _00.Work.MOON._01.Script.Player
             Vector3 localforward = transform.forward;
 
             localforward.y = 0;
-            localforward = localforward.normalized * (moveDir.y * moveSpeed);
+            localforward = localforward.normalized * (_moveDir.y * MoveSpeed);
 
-            changedEuler.y += moveDir.x;
+            changedEuler.y += _moveDir.x;
             float checkedCanMove = Vector3.Magnitude(new Vector3(_rb.angularVelocity.x, 0, _rb.angularVelocity.z));
-            if (checkedCanMove >= maxSpeed)
+            if (checkedCanMove >= MaxSpeed)
             {
                 IsMaxed = true;
-                localforward = 20.1f * localforward.normalized;
+                localforward = (MaxSpeed + 0.01f) * localforward.normalized;
                 changedVelocity = -localforward; 
             }
             else
@@ -59,7 +80,6 @@ namespace _00.Work.MOON._01.Script.Player
                 IsMaxed = false;
                 changedVelocity -= localforward; 
             }
-            print(checkedCanMove);
             transform.eulerAngles = changedEuler;
             _rb.angularVelocity = changedVelocity;
         }
