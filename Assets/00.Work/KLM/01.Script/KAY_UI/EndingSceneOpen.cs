1906e33e71f757f5e9db449236ad923e7eea9567
gdhsf3701 <gdhsf3701@gmail.com>
2025-05-26 11:34:59 +0900

    Fixed : EndScene

---
+++ Assets/00.Work/KLM/01.Script/KAY_UI/EndingSceneOpen.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _00.Work.KLM._01.Script.KAY_UI
+{
+    public class EndingSceneOpen : MonoBehaviour
+    {
+        private void Awake()
+        {
+            StartCoroutine(waitt());
+        }
+
+        IEnumerator waitt()
+        {
+            yield return new WaitForSeconds(5);
+            SceneManager.LoadScene("GongJang");
+        }
+    }
+}
