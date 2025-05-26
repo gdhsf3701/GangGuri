ace140b79e84d840cca6c8f3c4708bb66811f913
gdhsf3701 <gdhsf3701@gmail.com>
2025-05-26 17:00:32 +0900

    Feat : Particle

--- Assets/00.Work/JYE/01.Script/Obstacle/Hole/Hole.cs
+++ Assets/00.Work/JYE/01.Script/Obstacle/Hole/Hole.cs
@@ -4,10 +4,12 @@ namespace _00.Work.JYE._01.Script.Obstacle.Hole
 {
     public class Hole : MonoBehaviour
     {
+        [SerializeField] private ParticleSystem particle;
         [SerializeField] private Transform pos; //리스폰 될 위치
 
         private void OnTriggerEnter(Collider other)
         {
+            particle.Play();
             if (other.gameObject.CompareTag("Player"))
             {
                 other.transform.position = pos.position; //위치 이동
